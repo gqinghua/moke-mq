33c8fd4b3202ee804b4cb0e2f2e9d7947d67422b
郭清华 <h18239569764@163.com>
2020-11-07 21:08:01 +0800

    moke-mq

---
+++ src/main/java/com/moke/model/ResponseType.java
@@ -0,0 +1,12 @@
+package com.moke.model;
+
+
+
+/**
+ * broker的回应消息
+ */
+public enum ResponseType {
+    SendResult, //broker回应给生产者的ACK
+    Message, //消息 由broker发送给消费者的消息.
+    AckSubscript  //消费者订阅主题后,broker
+}
